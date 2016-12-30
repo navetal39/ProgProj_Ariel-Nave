d7974a2dda83b9e99e6b8764909281439c76188e
Nave <navetal@gmail.com>
2016-12-30 21:28:24 +0200

    Implemented (sort of) the main function
    Still need to implement the rest of the functions from main_aux and
    change this code if such a need will appear.

--- EX2/main.c
+++ EX2/main.c
@@ -1,9 +1,50 @@
 #include "SSPoint.h"
 #include "SSBPriorityQueue.h"
+#include "main_aux.h"
 #include <stdlib.h>
 #include <stdio.h>
 
 int main(int argc, char* argv)
 {
-	
+	/* Declerations: */
+	int n, d, k, index, pointsCount = 0;
+	int* nearest;
+	SPPoint** pointsArray;
+	SPPoint* targetPT;
+	/* Assertions, etc: */
+	if(scanf("%d %d %d", &n. &d, &k) != 3)
+	{
+		printf("ERROR: Expected 3 integer values seperated by spaces. Exiting...");
+		return -1;
+	}
+	assert(n>=1);
+	assert(d>=1);
+	assert(1<=k && k<=n);
+	/* Point data collection: */
+	pointsArray = readPoints(n,d, &pointsCount);
+	if(pointsArray = NULL)
+	{
+		printf("ERROR: An error occured while trying to read points. Exiting...");
+	}
+	targetPT = readPoint(d);
+	if(targetPT == NULL)
+	{
+		printf("ERROR: An error occured while trying to read target point. Exiting...");
+	}
+	/* Calculating: */
+	nearest = find_KNN(pointsArray, targetPT, n, d, k); ////////////
+	for(index = 0; index < k; ++index)
+	{
+		printf("%d", nearest[index]);
+		if(index<k-1)
+		{
+			printf(", ");
+		}else{
+			printf("\n"); // TODO: figure out if I need to add a \n or not.
+		}
+	}
+	/* Cleaning up: */
+	free(nearest);
+	spPointDestroy(targetPT);
+	cleanPointsArray(pointsArray, n); ///////////
 }
