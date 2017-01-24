a186df4d0a3d73268c8895d152effecefa5f7c88
Nave <navetal@gmail.com>
2017-01-24 21:07:51 +0200

    continued implementation of image processing utility functions file
    damn, that's a long name

--- EX3/sp_image_proc_util.cpp
+++ EX3/sp_image_proc_util.cpp
@@ -9,32 +9,70 @@ extern "C"{
 }
 using namespace cv;
 
+#define SIFT_FEATURE_DIM 128
+#define COLOR_NUM 3
+#define COLOR_RANGE_MAX 256
+#define COLOR_RANGE_MIN 0
+
 SPPoint** spGetRGBHist(const char* str,int imageIndex, int nBins)
 {
+	if(str==NULL || nBins<=0 || nBins>COLOR_RANGE_MAX)
+	{
+		return NULL;
+	}
 	Mat src;
-	Mat b_hist, g_hist, r_hist;
-	float range[] = {0, 256};
+	Mat hists[COLOR_NUM];
+	int i, j;
+	float range[] = {COLOR_RANGE_MIN, COLOR_RANGE_MAX};
 	const float* histRange = {range};
-	SPPoint* retArr[3];
-	float* data = (float*)malloc(nBins*sizeof(float));
+	SPPoint* retArr[COLOR_NUM];
+	float* data ;
 
 	src = imread(str, CV_LOAD_IMAGE_COLOR);
 	if(src.empty())
 	{
-		return -1; // TODO make sure it's good
+		return NULL;
 	}
 	std::vector<Mat> bgr_planes; // TODO help please
 	split(src, bgr_planes);
-	calcHist(&bgr_planes[0], 1, 0, Mat(), b_hist, 1, &nBins, &histRange);
-	calcHist(&bgr_planes[1], 1, 0, Mat(), g_hist, 1, &nBins, &histRange);
-	calcHist(&bgr_planes[2], 1, 0, Mat(), r_hist, 1, &nBins, &histRange);
-
+	calcHist(&bgr_planes[0], 1, 0, Mat(), hists[0], 1, &nBins, &histRange);
+	calcHist(&bgr_planes[1], 1, 0, Mat(), hists[1], 1, &nBins, &histRange);
+	calcHist(&bgr_planes[2], 1, 0, Mat(), hists[2], 1, &nBins, &histRange);
+	data = (float*)malloc(hists[0].rows * sizeof(float))
+	for(i=0; i<COLOR_NUM; i++)
+	{
+		for(j=0; j<hists[0].rows; j++)
+		{
+			data[j] = hists[i].at<float>(j,0);
+		}
+		retArr[i] = spPointCreate(data, hists[0].rows, imageIndex);
+	}
 	free(data);
 	return retArr;
 }
 double spRGBHistL2Distance(SPPoint** rgbHistA, SPPoint** rgbHistB)
 {
-	return 0.0;
+	int dims[2*COLOR_NUM] = {
+		spPointGetDimension(rgbHistA[0]),
+		spPointGetDimension(rgbHistB[0]),
+		spPointGetDimension(rgbHistA[1]),
+		spPointGetDimension(rgbHistB[1]),
+		spPointGetDimension(rgbHistA[2]),
+		spPointGetDimension(rgbHistB[2])};
+	int i;
+	for(i=1;i<2*COLOR_NUM;i++)
+	{
+		if(dims[i]!=dims[0])
+		{
+			return -1;
+		}
+	}
+	double distance;
+	for(i=0;i<COLOR_NUM;i++)
+	{
+		distance += 0.33*spPointL2SquaredDistance(rgbHistA[i], rgbHistB[i]);
+	}
+	return distance;
 }
 SPPoint** spGetSiftDescriptors(const char* str, int imageIndex, int nFeaturesToExtract, int *nFeatures)
 {
